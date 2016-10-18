79383ebf81e5ef68ba8c638f26c09e957a729a3b
Juan Nunez-Iglesias <jni.soma@gmail.com>
2016-10-18 14:17:17 +0300

    Create launch.sh
    How to launch the bigcat client to do interactive proofreading

---
+++ launch.sh
@@ -0,0 +1,22 @@
+
+BIGCAT_BRANCH=remote-client
+ID_SERVICE_BRANCH=master
+GALA_BRANCH=master
+
+### DOWNLOAD AND INSTALL
+
+# clone bigcat
+
+# mvn install dependencies
+
+# clone id-service
+
+# clone gala
+
+### RUN
+
+# run id-service
+
+# run gala-serve
+
+# run bigcat

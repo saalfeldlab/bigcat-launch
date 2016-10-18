6ab2f8b2deba56bd3e6bfd52ea6cd0e7db76a575
Juan Nunez-Iglesias <jni.soma@gmail.com>
2016-10-18 14:38:04 +0300

    Add gala install commands

--- launch.sh
+++ launch.sh
@@ -2,6 +2,7 @@
 BIGCAT_BRANCH=remote-client
 ID_SERVICE_BRANCH=master
 GALA_BRANCH=master
+CONDA_ENV_NAME=bigcat-launch
 
 ### DOWNLOAD AND INSTALL
 
@@ -11,7 +12,11 @@ GALA_BRANCH=master
 
 # clone id-service
 
-# clone gala
+# clone and install gala
+git clone -b $GALA_BRANCH https://github.com/jni/gala
+cd gala
+conda env create -n $CONDA_ENV_NAME
+pip install -e .
 
 ### RUN
 
